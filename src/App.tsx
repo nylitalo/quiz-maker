78ffd13c964b41f09b3ba67b6e6ad02eae224a16
Niklas Ylitalo <niklasylitalo@gmail.com>
2017-11-13 23:14:27 +0100

    Setup the project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+export interface Props {
+
+}
+
+export class App extends React.Component implements Props {
+    render() {
+        return (
+            <h1>Quiz Maker</h1>
+        )
+    }
+}
