21fd5219cca72bc551ce84a9fd9da82557089bfc
Niklas Ylitalo <niklasylitalo@gmail.com>
2017-11-17 21:12:33 +0100

    implement functionallity for adding a question to the quiz and storing each individual questions state.

--- src/components/CreateNewQuiz/CreateNewQuiz.tsx
+++ src/components/CreateNewQuiz/CreateNewQuiz.tsx
@@ -1,38 +1,63 @@
 import * as React from 'react';
 
-interface Props {}
-
-interface State {
-    questions: Object[]
-}
-
-export class CreateNewQuiz extends React.Component<Props, State> {
-    constructor(props:{}) {
+export class CreateNewQuiz extends React.Component<any, any> {
+    constructor(props: any) {
         super(props);
         this.state = {
             questions: [
+                {
+                    id: 0,
+                    question: '',
+                    answerType: '',
+                    answers: []
+                }
             ]
-        }
+        };
     }
 
-    createNewQuestion = () => {
+    createNewQuestion = (e: any) => {
+        e.preventDefault();
+        const newQuestion: {} = {
+            id: this.state.questions.length,
+            question: '',
+            answerType: '',
+            answers: []
+        };
         this.setState({
-        })
+            questions: [...this.state.questions, newQuestion]
+        });
+    }
+
+    handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        event.persist();
+        this.setState((prevState, props) => {
+            let newState = prevState;
+            newState.questions[event.target.id].question = event.target.value;
+            return {questions: newState.questions};
+        });
     }
     
-    render() {
+    render () {
         return(
             <div>
                 <form>
                     <label>Quiz name: </label>
                     <input type="text"/>
+                    <br/>
                     <label>Password</label>
                     <input type="text"/>
-                    /**
-                     * 
-                     */
+                    <br/>
+                    {this.state.questions.map((obj: any) => {
+                        return (
+                            <div key={obj.id}>
+                            <label>Question: </label>
+                            <input type="text" id={obj.id} value={obj.question} onChange={this.handleChange}/>
+                            </div>
+                        );
+                    })}
+                    <button onClick={this.createNewQuestion}>Add Question</button>
                 </form>         
             </div>
-        )
+        );
     }
 }
